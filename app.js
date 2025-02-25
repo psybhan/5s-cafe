d3b36dfe05467fc6f1475d21b620a167348d8b9c
psybhan <sobhan2034@gmail.com>
2025-02-24 21:59:17 -0500

    Update2 app.js

--- app.js
+++ app.js
@@ -1,11 +1,5 @@
 // JavaScript for handling form submissions
 
-// Reservation form
-document.getElementById('reservation-form')?.addEventListener('submit', function(e) {
-  e.preventDefault();
-  alert('Reservation submitted! We will contact you soon.');
-});
-
 // Contact form
 document.getElementById('contact-form')?.addEventListener('submit', function(e) {
   e.preventDefault();
